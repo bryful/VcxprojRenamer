80bbee836cdba430aa2d942fa41d75a61571067f
bryful <bryful@gmail.com>
2020-01-07 16:46:21 +0900

    プロジェクト ファイルを追加します。

---
+++ VcxprojRenamer/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace VcxprojRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lbTarget = new System.Windows.Forms.ListBox();
+            this.lbOrg = new System.Windows.Forms.Label();
+            this.tbOrg = new System.Windows.Forms.TextBox();
+            this.tbNew = new System.Windows.Forms.TextBox();
+            this.lbNew = new System.Windows.Forms.Label();
+            this.btnExec = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Select Folder";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.QuitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.aboutToolStripMenuItem.Text = "バージョン情報の表示";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 563);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(719, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lbTarget
+            // 
+            this.lbTarget.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTarget.FormattingEnabled = true;
+            this.lbTarget.ItemHeight = 12;
+            this.lbTarget.Location = new System.Drawing.Point(24, 207);
+            this.lbTarget.Name = "lbTarget";
+            this.lbTarget.Size = new System.Drawing.Size(683, 340);
+            this.lbTarget.TabIndex = 7;
+            // 
+            // lbOrg
+            // 
+            this.lbOrg.AutoSize = true;
+            this.lbOrg.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbOrg.Location = new System.Drawing.Point(22, 34);
+            this.lbOrg.Name = "lbOrg";
+            this.lbOrg.Size = new System.Drawing.Size(112, 16);
+            this.lbOrg.TabIndex = 0;
+            this.lbOrg.Text = "Target Folder";
+            // 
+            // tbOrg
+            // 
+            this.tbOrg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOrg.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbOrg.Location = new System.Drawing.Point(24, 101);
+            this.tbOrg.Name = "tbOrg";
+            this.tbOrg.Size = new System.Drawing.Size(500, 23);
+            this.tbOrg.TabIndex = 3;
+            this.tbOrg.TextChanged += new System.EventHandler(this.TbNew_TextChanged);
+            // 
+            // tbNew
+            // 
+            this.tbNew.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbNew.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNew.Location = new System.Drawing.Point(24, 147);
+            this.tbNew.Name = "tbNew";
+            this.tbNew.Size = new System.Drawing.Size(500, 23);
+            this.tbNew.TabIndex = 5;
+            this.tbNew.TextChanged += new System.EventHandler(this.TbNew_TextChanged);
+            // 
+            // lbNew
+            // 
+            this.lbNew.AutoSize = true;
+            this.lbNew.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbNew.Location = new System.Drawing.Point(22, 128);
+            this.lbNew.Name = "lbNew";
+            this.lbNew.Size = new System.Drawing.Size(72, 16);
+            this.lbNew.TabIndex = 4;
+            this.lbNew.Text = "New Name";
+            // 
+            // btnExec
+            // 
+            this.btnExec.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExec.Enabled = false;
+            this.btnExec.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnExec.Location = new System.Drawing.Point(543, 99);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(164, 69);
+            this.btnExec.TabIndex = 8;
+            this.btnExec.Text = "Rename Exec";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.BtnExec_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(21, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Target Files";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(22, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Orginal Name";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPath.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbPath.Location = new System.Drawing.Point(25, 53);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(682, 23);
+            this.tbPath.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 585);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExec);
+            this.Controls.Add(this.tbNew);
+            this.Controls.Add(this.lbNew);
+            this.Controls.Add(this.tbOrg);
+            this.Controls.Add(this.lbOrg);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lbTarget);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(380, 300);
+            this.Name = "Form1";
+            this.Text = "VcxprojRenamer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ListBox lbTarget;
+        private System.Windows.Forms.Label lbOrg;
+        private System.Windows.Forms.TextBox tbOrg;
+        private System.Windows.Forms.TextBox tbNew;
+        private System.Windows.Forms.Label lbNew;
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPath;
+    }
+}
+
---
+++ VcxprojRenamer/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Design;
+
+using BRY;
+
+using Codeplex.Data;
+/// <summary>
+/// 基本となるアプリのスケルトン
+/// </summary>
+namespace VcxprojRenamer
+{
+    public partial class Form1 : Form
+    {
+        private string m_path = "";
+        private List<string> m_TargetFiles = new List<string>();
+        //-------------------------------------------------------------
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// コントロールの初期化はこっちでやる
+        /// </summary>
+        protected override void InitLayout()
+        {
+            base.InitLayout();
+            lbTarget.Items.Clear();
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// フォーム作成時に呼ばれる
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //設定ファイルの読み込み
+            JsonPref pref = new JsonPref();
+            if (pref.Load())
+            {
+                bool ok = false;
+                Size sz = pref.GetSize("Size", out ok);
+                if (ok) this.Size = sz;
+                Point p = pref.GetPoint("Point", out ok);
+                if (ok) this.Location = p;
+                string s = pref.GetString("Path",out ok);
+                if (ok) m_path = s;
+            }
+            this.Text = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// フォームが閉じられた時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //設定ファイルの保存
+            JsonPref pref = new JsonPref();
+            pref.SetSize("Size", this.Size);
+            pref.SetPoint("Point", this.Location);
+            pref.SetString("Path", m_path);
+            pref.Save();
+
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// ドラッグ＆ドロップの準備
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        /// <summary>
+        /// ドラッグ＆ドロップの本体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //ここでは単純にファイルをリストアップするだけ
+            GetCommand(files);
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// ダミー関数
+        /// </summary>
+        /// <param name="cmd"></param>
+        public void GetCommand(string[] cmd)
+        {
+            m_TargetFiles.Clear();
+            lbTarget.Items.Clear();
+            tbPath.Text = "";
+            tbOrg.Text = "";
+            tbNew.Text = "";
+            btnExec.Enabled = false;
+
+            if (cmd.Length > 0)
+            {
+                foreach (string s in cmd)
+                {
+                    if (Directory.Exists(s) == false) continue;
+                    if (GetFilesFromFolder(s)==true)
+                    {
+                        break;
+                    }
+                }
+            }
+            if(m_TargetFiles.Count>0)
+            {
+                m_TargetFiles.Sort();
+                foreach (string s in m_TargetFiles)
+                {
+                    lbTarget.Items.Add(s);
+                }
+                tbPath.Text = Path.GetDirectoryName(m_TargetFiles[0]);
+                tbOrg.Text = tbNew.Text = Path.GetFileNameWithoutExtension(m_TargetFiles[0]);
+
+
+            }
+        }
+        /// <summary>
+        /// メニューの終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        //-------------------------------------------------------------
+        private void QuitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AppInfoDialog.ShowAppInfoDialog();
+        }
+        // **************************************************************************
+        private bool IsTargetFile(string p)
+        {
+            bool ret = false;
+
+            string e = Path.GetExtension(p);
+
+            ret = ((e == ".c") || (e == ".cpp") || (e == ".h") || (e == ".r")
+                || (e == ".vcxproj") || (e == ".filters")
+                || (e == ".plist") || (e == ".pbxproj") || (e == ".mode1v3") || (e == ".pbxuser")
+                || (e == ".xcworkspacedata") || (e == ".xcuserstate") || (e == ".xcsettings") || (e == "xcscheme"));
+
+            return ret;
+        }
+        // **************************************************************************
+        private int FindFile(string p)
+        {
+            int ret = -1;
+            int cnt = m_TargetFiles.Count;
+            if (cnt <= 0) return ret;
+            for ( int i=0; i<cnt;i++)
+            {
+                if( m_TargetFiles[i]==p)
+                {
+                    ret = i;
+                    break;
+                }
+            }
+            return ret;
+        }
+        // **************************************************************************
+        private void AddFile(string p)
+        {
+            if (IsTargetFile(p) == false) return;
+            int idx = FindFile(p);
+            if(idx<0)
+            {
+                m_TargetFiles.Add(p);
+            }
+        }
+        // **************************************************************************
+        private bool GetFilesFromFolder(string p)
+        {
+            bool ret = false;
+            if (Directory.Exists(p) == false) return ret;
+
+            string[] flist = Directory.GetFiles(p);
+            if(flist.Length>0)
+            {
+                for ( int i=0; i<flist.Length;i++)
+                {
+                    AddFile(flist[i]);
+                }
+            }
+
+            string[] dlist = Directory.GetDirectories(p);
+            if(dlist.Length>0)
+            {
+                for (int i = 0; i < dlist.Length; i++)
+                {
+                    string p2 = dlist[i];
+                    if ((p2 == ".") || (p2 == "..")) continue;
+                    GetFilesFromFolder(p2);
+                }
+            }
+            ret = true;
+            return ret;
+        }
+        // **************************************************************************
+        private void TbNew_TextChanged(object sender, EventArgs e)
+        {
+            string s = tbOrg.Text;
+            string d = tbNew.Text;
+            btnExec.Enabled = ((s != "")&& (d != "") && (s != d));
+        }
+        // **************************************************************************
+        private void BtnExec_Click(object sender, EventArgs e)
+        {
+            VcxprojConvert vc = new VcxprojConvert
+            {
+                SrcWord = tbOrg.Text,
+                DstWord = tbNew.Text
+            };
+            if ( vc.CanExec)
+            vc.TargetFiles = m_TargetFiles;
+            string[] p = new string[1];
+            string err = "";
+            p[0] = vc.ConvertExec(out err);
+            m_path = p[0];
+            GetCommand(p);
+            if (err != "")
+            {
+                MessageBox.Show(err);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            fbd.Description = "フォルダを指定してください。";
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.SelectedPath = m_path;
+
+            fbd.ShowNewFolderButton = false;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                string[] p = new string[1];
+                p[0] = fbd.SelectedPath;
+                GetCommand(p);
+            }
+        }
+    }
+}
